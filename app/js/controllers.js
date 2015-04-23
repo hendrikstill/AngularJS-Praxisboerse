b932c3d7c60359505f26f44b5484b27a74ac2384
Hendrik Still <hendrik.still@inovex.de>
2015-04-23 13:04:31 +0200

    Yolo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,9 @@ var praxisboerseControlellers = angular.module("praxisboerseControllers",[]);
 
 praxisboerseControlellers.controller("JobListCtrl",['$scope','$http','Job',
 function($scope,$http,Job){
-  /*
+
   Job.query({},function(jobs){
     $scope.jobs = jobs;
   });
-  */
-
-$http.get('https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/internship/0/-1',{
-    url:'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/internship/0/-1',
-    withCredentials: true,
-    headers: {
-      'x-requested-with':'XMLHttpRequest'
-    }
-}
-);
+  
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,12 +5,10 @@ var jobServices = angular.module('jobServices',['ngResource']);
 jobServices.factory('Job', ['$resource',
   function($resource){
 
-  
-
     return $resource('https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/internship/0/-1', {}, {
       query: {
         method:'GET',
-        //withCredentials:true,
+        withCredentials:true,
         transformResponse: function(data, headersGetter){
           var $offers = angular.fromJson(data).offers;
           return $offers;
