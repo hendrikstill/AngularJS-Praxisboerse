1fa2fb6e787a9033dbf070af87c9fe3f1954439b
Hendrik Still <hendrik.still@inovex.de>
2015-04-23 15:16:47 +0200

    Adds all job-attributes to job-list table

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,8 @@ var praxisboerseControlellers = angular.module("praxisboerseControllers",[]);
 praxisboerseControlellers.controller("JobListCtrl",['$scope','$http','Job',
 function($scope,$http,Job){
 
-  Job.query({},function(jobs){
-    $scope.jobs = jobs;
+  Job.query({typ:"workingstudent",start:"2",count:"2"},function(offers){
+    $scope.jobs = offers.offers;
   });
-  
+
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,15 +5,26 @@ var jobServices = angular.module('jobServices',['ngResource']);
 jobServices.factory('Job', ['$resource',
   function($resource){
 
-    return $resource('https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/internship/0/-1', {}, {
+    return $resource('https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/:typ/:key/:start/:count',
+    {typ:'internship',key:'',start:'0',count:'-1'},
+    {
       query: {
         method:'GET',
         withCredentials:true,
         transformResponse: function(data, headersGetter){
-          var $offers = angular.fromJson(data).offers;
-          return $offers;
-        },
-        isArray:true}
+          var apiResponse = angular.fromJson(data);
+          angular.forEach(apiResponse.offers,function(offer,key){
+            var companies = apiResponse.companies;
+            var offerTypes = apiResponse.offerTypes;
+            var countries = apiResponse.countries;
+
+            offer.companyName=companies[offer.companyId].companyName;
+            offer.type=offerTypes[offer.typeId].name;
+            offer.country=countries[offer.countryId].name;
+            offer.companyLocation=companies[offer.companyId].city;
+          });
+          return apiResponse;
+        }}
     });
   }]
 );
