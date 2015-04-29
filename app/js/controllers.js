763342b0e2f01d078dc19872242c33920bc3e09a
Thomas Walli <mail@thomas-walli.de>
2015-04-29 14:37:04 +0200

    disabled pagination for non mobile devices

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,10 @@
 
 var praxisboerseControlellers = angular.module("praxisboerseControllers",[]);
 
-praxisboerseControlellers.controller("JobListCtrl",['$scope','$http','Job','Country','Type',
+praxisboerseControlellers.controller("JobListCtrl",['$scope','$http', 'Job','Country','Type',
 function($scope,$http,Job,Country,Type){
   $scope.currentPage = 0;
-  
+
   Type.query({}, function(types) {
     $scope.types = types;
   });
@@ -14,7 +14,17 @@ function($scope,$http,Job,Country,Type){
   });
 
   $scope.change = function(){
-    Job.query({typ:$scope.typeQuery, key:$scope.textQuery, count:10, start: $scope.currentPage},function(offers){
+    var count = -1;
+    $scope.isMobile = false;
+    var userAgent = navigator.userAgent;
+
+    if (userAgent.indexOf("mobile") !== -1 || userAgent.indexOf("Android") !== -1|| userAgent.indexOf("iPhone") !== -1
+        || userAgent.indexOf("iPad") !== -1 || userAgent.indexOf("Windows Phone") !== -1) {
+          count = 10;
+          $scope.isMobile = true;
+    }
+
+    Job.query({typ:$scope.typeQuery, key:$scope.textQuery, count:count, start: $scope.currentPage},function(offers){
       $scope.totalHits = offers.totalHits;
       $scope.jobs = offers.offers;
     });
